4b2260b969a4c79c9c4412a03acde34eedb825cb
DorinaPetraru <dorinapetraruv@gmail.com>
2022-06-20 06:33:56 +0200

    README actualizado

--- db/serviceQueries/allServicesQuery.js
+++ db/serviceQueries/allServicesQuery.js
@@ -1,38 +1,20 @@
 const getConnection = require('../getConnection');
 
-const allServicesQuery = async (keyword) => {
+const allServicesQuery = async () => {
     let connection;
 
     try {
         connection = await getConnection();
 
-        let services;
-
-        // Si hay palabra clave "keyword" buscamos los services que contengan esa palabra
-        // clave. De lo contrario retornamos todos los services.
-        if (keyword) {
-            [services] = await connection.query(
-                `
-                    SELECT S.id, S.title, S.description, S.file,S.status, S.idUser, S.createdAt
-                    FROM services S
-                    LEFT JOIN users U 
-                    ON S.idUser = U.id
-                    WHERE S.description LIKE ?
-                    ORDER BY S.createdAt DESC
-                `,
-                [`%${keyword}%`]
-            );
-        } else {
-            [services] = await connection.query(
-                `
-                SELECT S.id, S.title, S.description, S.file,S.status, S.idUser, S.createdAt
+        const [services] = await connection.query(
+            `
+                SELECT S.id, S.title, S.description, S.file, S.statusService, S.idUser, S.createdAt
                 FROM services S
                 LEFT JOIN users U 
                 ON S.idUser = U.id
                 ORDER BY S.createdAt DESC
                 `
-            );
-        }
+        );
 
         return services;
     } finally {
